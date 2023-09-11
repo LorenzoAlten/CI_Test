3992e877c45eb2c6aa0b2ff77112b3cbf6d3a8e9
Lorenzo Bertini <lorenzo.bertini@alten.it>
2023-09-11 16:31:58 +0200

    Summary (required)

--- ProvaContinuous/Properties/Resources.Designer.cs
+++ ProvaContinuous/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TestCICD.Properties
+namespace ProvaContinuous.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace TestCICD.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TestCICD.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ProvaContinuous.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- ProvaContinuous/Properties/Settings.Designer.cs
+++ ProvaContinuous/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TestCICD.Properties
+namespace ProvaContinuous.Properties
 {
 
 
